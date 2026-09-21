ec3721bd8e2d8beba5f4cbe937e5c82b78f15b02
Gerit Wagner <g.wagner@fs.de>
2026-09-21 21:44:59 +0200

    update

--- materials/session_04/countries.py
+++ materials/session_04/countries.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-countries = pd.read_csv('../materials/data/countries.csv')
+countries = pd.read_csv('data/countries.csv')
 print(countries.shape)
 print(countries.columns) # or print(list(countries))
 countries.head()
